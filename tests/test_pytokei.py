ad796d6f72849e08cfd127bd5efa1c94fa4aeee2
agustin <agustin.piqueres@gmail.com>
2022-10-13 23:11:13 +0200

    tests: passing first tests for pylanguages

--- tests/test_pytokei.py
+++ tests/test_pytokei.py
@@ -19,6 +19,25 @@ class TestPyConfig:
     def test_columns(self, conf):
         assert conf.columns is None
 
+
+class TestPySort:
+    @pytest.fixture
+    def sort(self):
+        return tokei.PySort()
+
+    def test_repr(self, sort):
+        assert repr(sort) == "PySort(Lines)"
+
+    def test_from_str(self):
+        sort = tokei.PySort.from_str("blanks")
+        assert repr(sort) == "PySort(Blanks)"
+        sort = tokei.PySort.from_str("Blanks")
+        assert repr(sort) == "PySort(Blanks)"
+        # TODO: Control for undefined errors from rust side
+        # sort = tokei.PySort.from_str("undefined")
+        # assert repr(sort) == "PySort(Blanks)"
+
+
 class TestPyCodeStats:
     @pytest.fixture
     def stats(self):
@@ -44,5 +63,17 @@ class TestPyCodeStats:
 
 
 class TestPyLanguages:
-    def test_one(self):
-        pass
+    @pytest.fixture
+    def languages(self):
+        return tokei.PyLanguages()
+
+    def test_languages(self, languages):
+        assert isinstance(languages, tokei.PyLanguages)
+
+    def test_names(self, languages):
+        path = SAMPLE_FILES_PATH
+        print(path)
+        ignore = "nothing"  # This should take also a null string
+        conf = tokei.PyConfig()
+        languages.get_statistics(path, ignore, conf)
+        assert languages.language_names() == set(["Python", "Rust", "Dockerfile"])
