554d7515206251c44c34ab3fb64a80279a9a6823
agustin <agustin.piqueres@gmail.com>
2022-10-14 18:41:33 +0200

    feat: first version of language type maps

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,16 @@
 use pyo3::prelude::*;
 
 use crate::pyconfig::PyConfig;
-//use crate::pylanguage_type::PyLanguageType;
+use crate::pylanguage_type::{LanguageTypeContainer, language_types};
 use crate::pylanguages::PyLanguages;
 use crate::pysort::PySort;
-use crate::pystats::PyCodeStats;
+use crate::pystats::{PyCodeStats, PyReport};
 
 pub mod pyconfig;
 pub mod pylanguages;
 pub mod pysort;
 pub mod pystats;
+pub mod pylanguage_type;
 
 
 #[pymodule]
@@ -21,6 +22,9 @@ fn _pytokei(_py: Python, m: &PyModule) -> PyResult<()> {
 //    m.add_class::<PyLanguageType>().unwrap();
     m.add_class::<PySort>().unwrap();
     m.add_class::<PyCodeStats>().unwrap();
+    m.add_class::<PyReport>().unwrap();
+    m.add_function(wrap_pyfunction!(language_types, m)?)?;
+    m.add_class::<LanguageTypeContainer>().unwrap();
     Ok(())
     
 }
