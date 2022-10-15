ea7b896aeac0ba14882c09a798f079822e3a9f78
agustin <agustin.piqueres@gmail.com>
2022-10-15 10:23:17 +0200

    chore: formatted source code

--- pytokei/__init__.py
+++ pytokei/__init__.py
@@ -1,3 +1,21 @@
-from pytokei._pytokei import PyLanguages, PyConfig, PySort, PyCodeStats, language_types, LanguageTypeContainer, Report
+# from pytokei._pytokei import PyLanguages, PyConfig, PySort, PyCodeStats, language_types, LanguageTypeContainer, Report
+from pytokei._pytokei import (
+    CodeStats,
+    Config,
+    Languages,
+    LanguageTypeContainer,
+    Report,
+    Sort,
+    language_types,
+)
 
-__all__ = ["PyLanguages", "PyConfig", "PySort", "PyCodeStats", "language_types", "LanguageTypeContainer", "Report"]
+# __all__ = ["PyLanguages", "PyConfig", "PySort", "PyCodeStats", "language_types", "LanguageTypeContainer", "Report"]
+__all__ = [
+    "Languages",
+    "Config",
+    "Sort",
+    "CodeStats",
+    "language_types",
+    "LanguageTypeContainer",
+    "Report",
+]
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,25 @@
-
 use pyo3::prelude::*;
 
 use crate::pyconfig::PyConfig;
-use crate::pylanguage_type::{LanguageTypeContainer, language_types};
+use crate::pylanguage_type::{language_types, LanguageTypeContainer};
 use crate::pylanguages::PyLanguages;
 use crate::pysort::PySort;
 use crate::pystats::{PyCodeStats, PyReport};
 
 pub mod pyconfig;
+pub mod pylanguage_type;
 pub mod pylanguages;
 pub mod pysort;
 pub mod pystats;
-pub mod pylanguage_type;
-
 
 #[pymodule]
 fn _pytokei(_py: Python, m: &PyModule) -> PyResult<()> {
-
     m.add_class::<PyConfig>().unwrap();
     m.add_class::<PyLanguages>().unwrap();
-//    m.add_class::<PyLanguageType>().unwrap();
     m.add_class::<PySort>().unwrap();
     m.add_class::<PyCodeStats>().unwrap();
     m.add_class::<PyReport>().unwrap();
     m.add_function(wrap_pyfunction!(language_types, m)?)?;
     m.add_class::<LanguageTypeContainer>().unwrap();
     Ok(())
-    
 }
