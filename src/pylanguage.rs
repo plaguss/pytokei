26c114261182e9de5bda59c02c13c5722dc70e01
agustin <agustin.piqueres@gmail.com>
2022-10-15 17:32:52 +0200

    feat: first version of Language

---
+++ src/pylanguage.rs
@@ -0,0 +1,61 @@
+//use std::collections::{HashMap, HashSet};
+
+use pyo3::prelude::*;
+//use pyo3::types::PyString;
+
+use tokei::Language;
+
+//use crate::pyconfig::PyConfig;
+//use crate::pylanguage_type::LanguageTypeContainer;
+use crate::pystats::PyReport;
+
+#[pyclass(name = "Language")]
+pub struct PyLanguage {
+    pub language: Language,
+}
+
+#[pymethods]
+impl PyLanguage {
+    #[new]
+    pub fn new() -> Self {
+        PyLanguage {
+            language: Language::new(),
+        }
+    }
+
+    #[getter]
+    pub fn blanks(&self) -> usize {
+        self.language.blanks
+    }
+
+    #[getter]
+    pub fn code(&self) -> usize {
+        self.language.code
+    }
+
+    #[getter]
+    pub fn comments(&self) -> usize {
+        self.language.comments
+    }
+
+    #[getter]
+    pub fn reports(&self) -> Vec<PyReport> {
+        let mut reports = Vec::new();
+        let inner = self.language.reports.clone();
+        for r in &inner {
+            reports.push(PyReport{report: r.clone()});
+        }
+        reports
+    }
+    /*
+    #[getter]
+    pub fn children(&self) -> usize {
+        // TBD
+    }
+    */
+
+    #[getter]
+    pub fn innacurate(&self) -> bool {
+        self.language.inaccurate
+    }
+}
