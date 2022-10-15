0d6ca8f3d6ce322b0ad455a172f160cdfefd90e0
agustin <agustin.piqueres@gmail.com>
2022-10-15 09:34:12 +0200

    feat: hashmap creation more explicit

--- src/pystats.rs
+++ src/pystats.rs
@@ -50,11 +50,12 @@ impl PyCodeStats {
 
     pub fn content(&self) -> PyResult<PyObject> {
         // Obtain the inner content as a dict in Python.
-        let mut map = HashMap::new();
-        map.insert("blanks", self.blanks());
-        map.insert("code", self.code());
-        map.insert("comments", self.comments());
-        map.insert("lines", self.lines());
+        let map = HashMap::from([
+            ("blanks", self.blanks()),
+            ("code", self.code()),
+            ("comments", self.comments()),
+            ("lines", self.lines()),
+        ]);
         return pyo3::Python::with_gil( |py| {
             Ok( map.to_object( py ) )
         } );
@@ -73,9 +74,8 @@ pub struct PyReport {
 #[pymethods]
 impl PyReport {
     #[new]
-    pub fn py_new(name: &str) -> Self {
-        let mut path = PathBuf::new();
-        path.push(name);
+    pub fn new(name: &str) -> Self {
+        let path = PathBuf::from(name);
         PyReport{report: Report::new(path)}
     }
 }
